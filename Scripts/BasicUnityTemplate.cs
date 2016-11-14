c5bf1354a547d01a8361defd4eb6248a60f47472
RoseOfJared <RoseOfJared@outlook.com>
2016-11-13 22:06:57 -0500

    Starter Push
    - Added basic template for unity scripts
    - Added basic double jump script

---
+++ Scripts/BasicUnityTemplate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasicUnityTemplate : MonoBehaviour
+{
+	// Called when the script instance is being loaded
+	void Awake()
+	{
+		
+	}
+	
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	
+}
